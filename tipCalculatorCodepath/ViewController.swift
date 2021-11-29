a7032e7f8b3acb55c05ecced319f2fa8992e3013
jeadamcho <59285101+jeadamcho@users.noreply.github.com>
2021-11-28 22:01:22 -0800

    test

--- tipCalculatorCodepath/ViewController.swift
+++ tipCalculatorCodepath/ViewController.swift
@@ -19,5 +19,17 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(billAmounTextField.text!) ?? 0
+        
+        let tipPercentages = [0.15, 0.18, 0.20]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+    
 }
 
