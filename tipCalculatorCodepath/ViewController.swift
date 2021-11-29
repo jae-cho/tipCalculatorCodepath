da09bdc71c5ebb12dfaca6f7e6a0b3b66cf37da1
jeadamcho <59285101+jeadamcho@users.noreply.github.com>
2021-11-28 21:57:19 -0800

    test

--- tipCalculatorCodepath/ViewController.swift
+++ tipCalculatorCodepath/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var billAmounTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
